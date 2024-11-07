0d85b56da2cd9adda3e66e8c7ed40c86b03fc0cc
andrea <andreafarfaglia2002@gmail.com>
2024-11-07 16:40:23 +0100

    ricerca funzionante e anche lo spinner sembra okay

--- 01-starting-project/src/Components/Prodotti.jsx
+++ 01-starting-project/src/Components/Prodotti.jsx
@@ -1,60 +1,94 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import classes from "../ComponentsCss/Ricerca.module.css"
+import classes from "../ComponentsCss/Ricerca.module.css";
+import { LoadingContext } from "../GlobalContext/LoadingContext";
+import spinnerStyles from "../ComponentsCss/Spinner.module.css";
 
 import axios from "axios";
 
+export default function Prodotti({ searchText }) {
+  const { isLoading, startLoading, stopLoading } = useContext(LoadingContext);
 
-export default function Prodotti({ searchText }){
-    const [products, setProducts] = useState([]);
-   
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            if(searchText=="vetrina"){
-                try {
-                    const response = await axios.get("http://localhost:8080/prodotto/elencaVetrina");
-                    setProducts(response.data); 
-                } catch (error) {
-                    console.error("Errore nel recupero dei prodotti:", error);
-                }
-            }
-            else{
-                try {
-                    console.log("ci siamo")
-                    const response = await axios.get(`http://localhost:8080/prodotto/getProdotti?prod=${searchText}`);
-                    setProducts(response.data); 
-                    console.log(response)
-                    console.log("ci siamo2")
-                } catch (error) {
-                    console.error("Errore nel recupero dei prodotti:", error);
-                }
-            }
-        };
+  const handleLoading = () => {
+    startLoading();
+  };
 
-        fetchProducts();
-    }, []);
+  const endLogin = () => {
+    stopLoading();
+  };
 
-    
-    return (
+  useEffect(() => {
+    const fetchProducts = async () => {
+      handleLoading();
+      if (searchText == "vetrina") {
+        try {
+          const response = await axios.get(
+            "http://localhost:8080/prodotto/elencaVetrina"
+          );
+          setProducts(response.data);
+          endLogin();
+        } catch (error) {
+          console.error("Errore nel recupero dei prodotti:", error);
+        }
+      } else {
+        try {
+          console.log(searchText);
+          const response = await axios.get(
+            `http://localhost:8080/prodotto/getProdotti?prod=${searchText}`
+          );
+          setProducts(response.data);
+          endLogin();
+        } catch (error) {
+          console.error("Errore nel recupero dei prodotti:", error);
+        }
+      }
+    };
+
+    fetchProducts();
+  }, [searchText]);
+
+  return (
+    <main className={isLoading ? spinnerStyles.blurred : ""}>
+      {isLoading && (
+        <div className={spinnerStyles["spinner-overlay"]}>
+          <div className={spinnerStyles.spinner}></div>
+        </div>
+      )}
+
+      {!isLoading && (
         <div className={classes["products-grid"]}>
-        {products.map((product) => (
-            
+          {products.map((product) => (
             <div className={classes["product-item"]}>
-                
-                <Link to={{ pathname: `/product/${product.id}` }} state={{ product }} key={product.id}>
-
-                    <button   >
-                        <img src={`/images/${product.immagini.split(',')[0]}`} alt={product.nome} />
-                        <div className={classes["product-info"]}>
-                            <h3>{product.nome}</h3>
-                            <p style={{ fontWeight: 'bold' }}>${Number(product.prezzo).toFixed(2)}</p>
-                            
-                        </div>
-                    </button>
-                </Link>
+              <Link
+                to={{ pathname: `/product/${product.id}` }}
+                state={{ product }}
+                key={product.id}
+              >
+                <button>
+                  <img
+                    src={`/images/${product.immagini.split(",")[0]}`}
+                    alt={product.nome}
+                  />
+                  <div className={classes["product-info"]}>
+                    <h3>{product.nome}</h3>
+                    <p style={{ fontWeight: "bold" }}>
+                      ${Number(product.prezzo).toFixed(2)}
+                    </p>
+                  </div>
+                </button>
+              </Link>
             </div>
-        ))}
-    </div>
-    );
+          ))}
+          
+        </div>
+        
+      )}
+        {!isLoading && products.length<=0 &&
+          <div className={classes["noRes"]}>La ricerca non ha prodotto alcun resultato</div>
+          }
+
+    </main>
+  );
 }
--- 01-starting-project/src/Components/Ricerca.jsx
+++ 01-starting-project/src/Components/Ricerca.jsx
@@ -5,24 +5,12 @@ import { useContext, useEffect } from "react";
 import spinnerStyles from "../ComponentsCss/Spinner.module.css";
 
 export default function Ricerca({ testo }) {
-    const { isLoading, startLoading, stopLoading } = useContext(LoadingContext);
-
-    const handleLoading = () => {
-        startLoading();
-        setTimeout(() => stopLoading(), 1000); // Simula il caricamento
-    };
 
     useEffect(() => {
     }, [testo]); // Rende l'effetto reattivo al cambiamento di `testo`
 
     return (
-        <main className={isLoading ? spinnerStyles.blurred : ''}>
-            {isLoading && (
-                <div className={spinnerStyles["spinner-overlay"]}>
-                    <div className={spinnerStyles.spinner}></div>
-                </div>
-            )}
-            {!isLoading && (
+        
                 <>
                     <div style={{ marginBottom: '1.5rem' }}>
                         <div className={classes["h2"]} style={{ fontSize: '1.5rem', fontWeight: 'bold', margin: 0 }}>
@@ -31,7 +19,6 @@ export default function Ricerca({ testo }) {
                     </div>
                     <Prodotti searchText={testo} />
                 </>
-            )}
-        </main>
+            
     );
 }
